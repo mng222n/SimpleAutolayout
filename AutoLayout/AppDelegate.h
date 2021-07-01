9f9cb53575f604db2c5c42e6904886cc5844221d
Minh Nguyen <anh.minh.anh.nguyen@gmail.com>
2021-07-01 18:06:13 +0700

    add code autolayout

---
+++ AutoLayout/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AutoLayout
+//
+//  Created by Ziad on 6/13/14.
+//  Copyright (c) 2014 AppCoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AutoLayout/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AutoLayout
+//
+//  Created by Ziad on 6/13/14.
+//  Copyright (c) 2014 AppCoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
